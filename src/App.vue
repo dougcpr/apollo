1cce3bd5e448629e54c1edbf1b3d8f1d1df4a538
Doug Cooper <doug.cpr@gmail.com>
2018-02-24 18:46:41 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  v-app
+    v-content
+      <router-view/>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [{
+        icon: 'bubble_chart',
+        title: 'Inspire'
+      }],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    }
+  },
+  name: 'App'
+}
+</script>
